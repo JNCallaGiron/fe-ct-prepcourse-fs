e813ae02b490414b7108c4172ab6717f2c08d119
JNCallaGiron <jairocalla3@gmail.com>
2023-11-23 00:05:57 -0300

    avances 05

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -49,48 +49,40 @@ function dePalabrasAFrase(palabras) {
    // con un espacio entre cada palabra.
    // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'.
    // Tu código:
-   return palabras.join(' ')
+      return  palabras.join( ' ')
 }
 
 function arrayContiene(array, elemento) {
    // Verifica si el elemento existe dentro del arreglo recibido.
    // Retornar true si está, o false si no está.
    // Tu código:
-    return array.includes(elemento)
+
+   return array.includes(elemento) // con respecto a la clase este metodo es el mas optimo 
 }
 
 function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
    // Suma todos los elementos y retorna el resultado.
    // Tu código:
-   suma = 0; 
-
-   for( var i = 0; i < arrayOfNums.length; i++){
-      suma += arrayOfNums[i]
-   }return suma 
+   return arrayOfNums.reduce(function(acum, elemento) {
+      acum = acum + elemento
+      return acum 
+   })
 }
 
 function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
-
-      if (resultadosTest.length === 0) {
-        return 0; // Manejar el caso en el que el array esté vacío para evitar dividir por 0
-      }
-    
-      let suma = 0;
-    
-      for (let i = 0; i < resultadosTest.length; i++) {
-        suma += resultadosTest[i];
-      }
-    
-      return suma / resultadosTest.length;
-    }
+   var result = resultadosTest.reduce(function(acum, ele) {
+      return acum = acum + ele
+   })
+      return result / resultadosTest.length; 
+  }
     
 
 function numeroMasGrande(arrayOfNums) {
-   // El parámetro "arrayOfNums" es un arreglo de números.
+   // El parámetro "arrayOfNums" es un arreglo de números.   
    // Retornar el número más grande.
    // Tu código:
 
@@ -102,12 +94,15 @@ function multiplicarArgumentos() {
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    // Tu código:
+
    if (arguments.length == 0){
       return 0;
    }
    var  producto = 1
  for (var  i = 0; i < arguments.length; i++){
-   producto *= arguments[i]
+
+   producto = producto * arguments[i]
+
  } return producto
 }
 
@@ -115,18 +110,19 @@ function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
    // Tu código:
 
-      var  cantidad = 0;
+      var  contador = 0;
     
       for (let i = 0; i < array.length; i++) {
         if (array[i] > 18) {
-          cantidad ++
-        }
+          contador ++
+       
+         }
       }
-    
-      return cantidad;
+
+      return contador;
     }
    
-    
+
 
 function diaDeLaSemana(numeroDeDia) {
    // Supongamos que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente.
@@ -146,19 +142,29 @@ function empiezaConNueve(num) {
    // Esta función recibe por parámetro un número.
    // Debe retornar true si el entero inicia con 9 y false en otro caso.
    // Tu código:
+   return num.toString() .startsWith('9');
 }
 
 function todosIguales(array) {
    // Si todos los elementos del arreglo son iguales, retornar true.
    // Caso contrario retornar false.
    // Tu código:
-}
+  var primero = array[0]
+  for ( var i = 0; i < array.length; i++ ){
+   if (primero !== array[i]){ 
+   return false 
+   } 
+ }
+  return true; 
+} 
 
 function mesesDelAño(array) {
    // El arreglo contiene algunos meses del año desordenados. Debes recorrerlo, buscar los meses "Enero",
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
+   
+  
 }
 
 function tablaDelSeis() {
